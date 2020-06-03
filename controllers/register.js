f675520ddb5fcc7ceef35d557c2a9b83c4d1543c
themikaelson <tranquang2407@gmail.com>
2020-06-03 15:53:11 -0400

    upload smart-brain-api

---
+++ controllers/register.js
@@ -0,0 +1,39 @@
+
+const handleRegister = (req, res, db, bcrypt) => {
+	const { email, name, password } = req.body;
+	// const saltRounds = 10;
+	if( !email || !name || !password ) {
+		return res.status(400).json('incorrect form submission !!');
+	}
+	const hash = bcrypt.hashSync(password);
+console.log('asdads');
+
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					})
+						.into('users')
+						.returning('*')
+						.then(user => {
+							res.json(user[0]);
+						})
+			})
+			.then(trx.commit)
+			.catch(trx.rolllback)	
+	})
+		.catch(err => res.status(400).json('unable to register'));
+}
+
+
+module.exports = {
+	handleRegister
+};
