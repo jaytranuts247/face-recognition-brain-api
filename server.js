c6997d723bca6895f2180ac7d37c15657947537b
themikaelson <34260590+themikaelson@users.noreply.github.com>
2020-06-03 16:26:19 -0400

    adding port

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/imageurl', (req,res) => image.handleAPICall(req, res));
 //     // res = false
 // });
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
