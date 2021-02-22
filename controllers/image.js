24526e0d17e2724c4744c0a6dfec679bc8b7f665
themikaelson <34260590+themikaelson@users.noreply.github.com>
2021-02-22 18:04:04 -0500

    change models number

--- controllers/image.js
+++ controllers/image.js
@@ -7,9 +7,10 @@ const app = new Clarifai.App({
 const handleAPICall = (req, res) => {
 	console.log("handleAPICall", Clarifai.FACE_DETECT_MODEL, process.env.APIKEY);
 	app.models
-		.predict(Clarifai.FACE_DETECTION_MODEL, req.body.input)
+		// .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 		//53e1df302c079b3db8a0a36033ed2d15
 		// a403429f2ddf4b49b307e318f00e528b
+		// c0c0ac362b03416da06ab3fa36fb58e3
 		// .predict(
 		// 	{
 		// 		id: "53e1df302c079b3db8a0a36033ed2d15",
@@ -17,6 +18,7 @@ const handleAPICall = (req, res) => {
 		// 	},
 		// 	req.body.input
 		// )
+		.predict("c0c0ac362b03416da06ab3fa36fb58e3", req.body.input)
 		.then((data) => {
 			res.json(data);
 		})
